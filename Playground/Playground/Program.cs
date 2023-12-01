38ac83c101cd7b9d7cf8a2835fca574200f23952
Zvonimir Grubisic <zvonimir.grubisic@avanade.com>
2023-12-01 10:48:38 +0100

    update playground to net7

--- Playground/Playground/Program.cs
+++ Playground/Playground/Program.cs
@@ -1,25 +1,19 @@
-﻿using System;
+﻿using Playground;
 
-namespace Playground
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                ////// braces
-                //Console.WriteLine("Input braces string:");
-                //var braces = Console.ReadLine();
-                //Console.WriteLine($"This brace string is:\t{Braces.Validate(braces)}");
 
-                ////// morse code letter guesser thingy
-                ////// use ? to replace . or -
-                Console.WriteLine("Input morse code:");
-                var letterSigns = Console.ReadLine();
-                Console.WriteLine($"Possible letters: " +
-                    $"{String.Join(",", MorseCode.GetPossibleMatches(letterSigns))}");
-            }
-        }
-    }
-}
+
+// other
+// while (true)
+// {
+//     ////// braces
+//     //Console.WriteLine("Input braces string:");
+//     //var braces = Console.ReadLine();
+//     //Console.WriteLine($"This brace string is:\t{Braces.Validate(braces)}");
+//
+//     ////// morse code letter guesser thingy
+//     ////// use ? to replace . or -
+//     Console.WriteLine("Input morse code:");
+//     var letterSigns = Console.ReadLine();
+//     Console.WriteLine($"Possible letters: " +
+//         $"{String.Join(",", MorseCode.GetPossibleMatches(letterSigns))}");
+// }
